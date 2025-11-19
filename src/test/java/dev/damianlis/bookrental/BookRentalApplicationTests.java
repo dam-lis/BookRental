a8505e60892e8e607af6cbaac60241c383b5ca7c
dam-lis <dmnls@proton.me>
2025-11-19 20:44:28 +0100

    spring boot init

---
+++ src/test/java/dev/damianlis/bookrental/BookRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.damianlis.bookrental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookRentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
